0005f5fa6f21cc7bb2c3b0391636fedefde79c84
LeeKangJin <himnae5181@naver.com>
2020-02-15 17:23:09 +0900

    GUI update

--- GetRealEstateTradeData.py
+++ GetRealEstateTradeData.py
@@ -2,44 +2,53 @@ from base64 import encode, decode
 from urllib.request import Request, urlopen
 from numpy.core import unicode
 
+class GetRealEstateTradeData:
 
-
-input ='인천 강화'
-input2 = "201512"
-inputs = input.split(' ')
-sido = str(inputs[0].encode('utf-8'))
-gungo =str(inputs[1].encode('utf-8'))
-# munli =str(inputs[2])
-# number=str( inputs[3])
-# dong =str( inputs[4])
-# ho =      str( inputs[5])
-
-url = "http://openapi.1365.go.kr/openapi/service/rest/CodeInquiryService/getAreaCodeInquiryList?schSido="+sido+"&schGugun="+gungo
-
-urlEx = "http://openapi.1365.go.kr/openapi/service/rest/CodeInquiryService/getAreaCodeInquiryList?schSido=%EC%84%9C%EC%9A%B8&schGugun=%EC%A2%85%EB%A1%9C"
-
-request = Request(urlEx)
-request.get_method = lambda: 'GET'
-response_body = urlopen(request).read()
-print(response_body)
-
-
-# 코드값 가져오기
+    def getCode(self,sido,gungo):
+        url = "http://openapi.1365.go.kr/openapi/service/rest/CodeInquiryService/getAreaCodeInquiryList?schSido="+sido+"&schGugun="+gungo
+        request = Request(urlEx)
+        request.get_method = lambda: 'GET'
+        response_body = urlopen(request).read()
+        return response_body
 
 
 
-# 진짜 URL 던지기
+# input ='인천 강화'
+# input2 = "201512"
+# inputs = input.split(' ')
+# sido = str(inputs[0].encode('utf-8'))
+# gungo =str(inputs[1].encode('utf-8'))
+# # munli =str(inputs[2])
+# # number=str( inputs[3])
+# # dong =str( inputs[4])
+# # ho =      str( inputs[5])
 
-code = response_body 중에 지역코드를 짤라냄
+# url = "http://openapi.1365.go.kr/openapi/service/rest/CodeInquiryService/getAreaCodeInquiryList?schSido="+sido+"&schGugun="+gungo
+
+# urlEx = "http://openapi.1365.go.kr/openapi/service/rest/CodeInquiryService/getAreaCodeInquiryList?schSido=%EC%84%9C%EC%9A%B8&schGugun=%EC%A2%85%EB%A1%9C"
+
+# request = Request(urlEx)
+# request.get_method = lambda: 'GET'
+# response_body = urlopen(request).read()
+# print(response_body)
+
+
+# # 코드값 가져오기
 
 
 
-url = "~~ KK "+ code + input2
+# # 진짜 URL 던지기
+
+# code = response_body 중에 지역코드를 짤라냄
 
 
-request = Request(url)
-request.get_method = lambda: 'GET'
-response_body = urlopen(request).read()
 
-response_body.t
+# url = "~~ KK "+ code + input2
+
+
+# request = Request(url)
+# request.get_method = lambda: 'GET'
+# response_body = urlopen(request).read()
+
+# response_body.t
 
--- InsertData.py
+++ InsertData.py
@@ -1,31 +1,38 @@
 # ## Author : Lee Kang jin
 # ## Description : Input - Location Type
 
-import tkinter
+import tkinter 
+from tkinter import messagebox
+import GetRealEstateTradeData as gt
 
 def process_start():
-    # main logic 실행
-    count = 0
+    inputText = text.get("1.0","end")
+    inputTextParsing = str(inputText).split(' ')
 
+    response_body = gt.GetRealEstateTradeData.getCode(inputTextParsing[0].encode('utf-8') , inputTextParsing[1].encode('utf-8') )
+    print(response_body)
+
+    
 mainGUI = tkinter.Tk()
-
-
 
 mainGUI.title("경매 데이터 분석")
 mainGUI.geometry("400x200+0+0")
 
-label = tkinter.Label(mainGUI, text="분석하고자 하는 주소를 입력하세요")
-label.pack() 
-text=tkinter.Text(mainGUI,height =3,width = 20)
+label = tkinter.Label(mainGUI, text="주소를 입력:")
 
+# label.grid(row=0, column=0)
+label.pack() 
+
+text = tkinter.Text(mainGUI,height =1,width = 20)
+# text.grid(row =0, colum = 1)
 text.pack()
 
 
-btn1 = tkinter.Button(mainGUI, text="검색", command=process_start)
-btn1.pack()
-
-
+btnSearch = tkinter.Button(mainGUI, text="완료", command=process_start)
+# btnSearch.grid(row =1, colum =1)
+btnSearch.pack()
 
 
 mainGUI.mainloop()
+
 
